0ef7efaf5a021639fa3c022e86c3e60acbdc3b8f
Khatai Alizade <qaqaw4930@gmail.com>
2024-02-25 20:31:10 +0100

    New Commit

--- src/HavaTask1.java
+++ src/HavaTask1.java
@@ -1,7 +1,18 @@
 public class HavaTask1 {
     public static void main(String[] args) {
+        System.out.println("Starting the program...");
         System.out.println("Hello World");
-        System.out.println("Hello Java");
+        System.out.println("Debugging starts here...");
 
+        // Set a breakpoint on the next line
+        int result = addNumbers(5, 7);
+
+        System.out.println("Result of adding numbers: " + result);
+        System.out.println("End of the program.");
+    }
+
+    public static int addNumbers(int a, int b) {
+        System.out.println("Adding numbers: " + a + " and " + b);
+        return a + b;
     }
 }
