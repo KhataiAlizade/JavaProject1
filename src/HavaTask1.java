a5f6fdc9cef73ee628ddc877fbc9eb10838c0fe3
Khatai Alizade <qaqaw4930@gmail.com>
2024-02-25 20:29:45 +0100

    New Commit

--- src/HavaTask1.java
+++ src/HavaTask1.java
@@ -1,7 +1,7 @@
 public class HavaTask1 {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("Hello World");
+        System.out.println("Hello Java");
 
     }
 }
